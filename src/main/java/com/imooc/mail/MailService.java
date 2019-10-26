21f99e197892428cf7a5be4a20a1dd5d3089b0f9
liujian <1173171092@qq.com>
2019-10-26 20:33:21 +0800

    测试方法

--- src/main/java/com/imooc/mail/MailService.java
+++ src/main/java/com/imooc/mail/MailService.java
@@ -2,9 +2,16 @@ package com.imooc.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.HashMap;
 
 @Service
 public class MailService {
@@ -15,6 +22,12 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    /**
+     * 发送普通邮件
+     * @param to
+     * @param subject
+     * @param context
+     */
     public void sendSimpleMail(String to,String subject,String context){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom(from);//发件人
@@ -22,5 +35,50 @@ public class MailService {
         mailMessage.setSubject(subject);//主题
         mailMessage.setText(context);//邮件内容
         mailSender.send(mailMessage);//发送
+    }
+     public void sendHtmlMail(String to,String subject,String context) throws Exception {
+         MimeMessage mimeMessage = mailSender.createMimeMessage();
+         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,true);
+         messageHelper.setTo(to);
+         messageHelper.setFrom(from);
+         messageHelper.setSubject(subject);
+         messageHelper.setText(context,true);
+         mailSender.send(mimeMessage);
+     }
+
+    /**
+     * 发送带附件的邮件
+     * @param to
+     * @param subject
+     * @param context
+     * @param file
+     * @throws MessagingException
+     */
+    public void sendFileMail(String to, String subject, String context, HashMap file) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,true);
+
+        // 添加多个附件
+        for(Object key:file.keySet()){
+            FileSystemResource resource = new FileSystemResource(file.get(key).toString());
+            messageHelper.addAttachment(resource.getFilename(),resource);
+        }
+        messageHelper.setFrom(from);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(context,true);
+        mailSender.send(mimeMessage);
+    }
+
+    public void sendImgMail(String to, String subject, String context,FileSystemResource resource,String rscId) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,true);
+
+        messageHelper.setFrom(from);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(context,true);
+        messageHelper.addInline(rscId,resource);
+        mailSender.send(mimeMessage);
     }
 }
--- src/test/java/com/imooc/mail/service/MailTest.java
+++ src/test/java/com/imooc/mail/service/MailTest.java
@@ -3,11 +3,13 @@ package com.imooc.mail.service;
 import com.imooc.mail.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,5 +22,56 @@ public class MailTest {
         String subject = "这是一封springboot邮件";
         String context = "这是一封测试邮件";
         mailService.sendSimpleMail("1173171092@qq.com",subject,context);
+    }
+
+    @Test
+    public void sendHtmlMailTest(){
+        String subject = "这是一封springboot邮件";
+        String context = "<html>" +
+                "<body>" +
+                "<h1>" +"这是一个H1标题"+
+                "</h1>" +
+                "</body>" +
+                "</html>";
+        try {
+            mailService.sendHtmlMail("1173171092@qq.com",subject,context);
+        }catch (Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    @Test
+    public void sendFileMailTest(){
+        String subject = "这是一封springboot邮件";
+        String context = "<html>" +
+                "<body>" +
+                "<h1>" +"这是一个H1标题"+
+                "</h1>" +
+                "</body>" +
+                "</html>";
+        HashMap<Object,String> file = new HashMap<Object, String>();
+        file.put(1,"/Users/cj/Documents/面试/面试题整理.docx");
+        file.put(2,"/Users/cj/Documents/面试/模板.docx");
+        try{
+            mailService.sendFileMail("1173171092@qq.com",subject,context,file);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void sendImgMailTest(){
+        String image = "/Users/cj/Downloads/1568045440.png";
+        FileSystemResource resource = new FileSystemResource(new File(image));
+        String subject = "这是一封springboot邮件";
+        String rscId = resource.getFilename().substring(0,resource.getFilename().indexOf("."));
+        String context = "<html>" +
+                "<body><h1>这是一个H1标题</h1>:<img src=\'cid:" +
+                rscId + "\'></img></body></html>";
+        try{
+            mailService.sendImgMail("1173171092@qq.com",subject,context,resource,rscId);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
